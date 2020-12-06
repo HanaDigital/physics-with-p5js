9ebbd9c5350d4eacabdc180323e128e30a0008b1
Shehryar <shehryar.dev@gmail.com>
2020-12-06 09:24:59 +0400

    marching squares

---
+++ marching-squares/sketch.js
@@ -0,0 +1,111 @@
+const rez = 20;
+let cols, rows;
+const field = [];
+const seed = Date.now();
+const openSimplex = openSimplexNoise(seed);
+const increment = 0.1;
+let zoff = 0;
+
+function setup() {
+	createCanvas(800, 800);
+	cols = 1 + width / rez;
+	rows = 1 + height / rez;
+}
+
+function draw() {
+	background(0);
+
+	let xoff = 0;
+	let yoff = 0;
+	for (let i = 0; i < cols; i++) {
+		xoff += increment;
+		yoff = 0;
+		field[i] = [];
+		for (let j = 0; j < height; j++) {
+			field[i][j] = openSimplex.noise3D(xoff, yoff, zoff);
+			yoff += increment;
+		}
+	}
+	zoff += 0.1;
+
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < height; j++) {
+			stroke(field[i][j] * 255);
+			strokeWeight(rez * 0.4);
+			point(i * rez, j * rez);
+		}
+	}
+
+	stroke(255);
+	strokeWeight(1);
+	for (let i = 0; i < cols - 1; i++) {
+		for (let j = 0; j < height - 1; j++) {
+			let x = i * rez;
+			let y = j * rez;
+			let a = createVector(x + rez * 0.5, y);
+			let b = createVector(x + rez, y + rez * 0.5);
+			let c = createVector(x + rez * 0.5, y + rez);
+			let d = createVector(x, y + rez * 0.5);
+			let state = getState(
+				ceil(field[i][j]),
+				ceil(field[i + 1][j]),
+				ceil(field[i + 1][j + 1]),
+				ceil(field[i][j + 1])
+			);
+			switch (state) {
+				case 1:
+					lineGen(c, d);
+					break;
+				case 2:
+					lineGen(b, c);
+					break;
+				case 3:
+					lineGen(b, d);
+					break;
+				case 4:
+					lineGen(a, b);
+					break;
+				case 5:
+					lineGen(a, d);
+					lineGen(b, c);
+					break;
+				case 6:
+					lineGen(a, c);
+					break;
+				case 7:
+					lineGen(a, d);
+					break;
+				case 8:
+					lineGen(a, d);
+					break;
+				case 9:
+					lineGen(a, c);
+					break;
+				case 10:
+					lineGen(a, b);
+					lineGen(c, d);
+					break;
+				case 11:
+					lineGen(a, b);
+					break;
+				case 12:
+					lineGen(b, d);
+					break;
+				case 13:
+					lineGen(b, c);
+					break;
+				case 14:
+					lineGen(c, d);
+					break;
+			}
+		}
+	}
+}
+
+function getState(a, b, c, d) {
+	return a * 8 + b * 4 + c * 2 + d * 1;
+}
+
+function lineGen(v1, v2) {
+	line(v1.x, v1.y, v2.x, v2.y);
+}
